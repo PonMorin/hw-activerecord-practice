856208b56226f2e2b5543d76cca80107e76107be
Pon <pon.m22@gmail.com>
2023-01-16 23:30:47 +0700

    final

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -26,4 +26,37 @@ class Customer < ActiveRecord::Base
     where("email LIKE ?", "%@%")
   end
   # etc. - see README.md for more details
+  def self.with_dot_org_email
+    where("email LIKE ?", "%\.org%")
+  end
+  def self.with_invalid_email
+    where("email NOT LIKE ?", "%@%")
+  end
+  def self.with_blank_email
+    where(email: nil)
+  end
+  def self.born_before_1980
+    where("birthdate < ?", "1980-01-01")
+  end
+  def self.with_valid_email_and_born_before_1980
+    where("email LIKE ? AND birthdate < ?", "%@%", "1980-01-01")
+  end
+  def self.last_names_starting_with_b
+    where("last LIKE ?", "B%").order(birthdate: :asc)
+  end
+  def self.twenty_youngest
+    order(birthdate: :desc).limit(20)
+  end
+  def self.update_gussie_murray_birthdate
+    where(first: "Gussie").update(birthdate: "2004-02-08")
+  end
+  def self.change_all_invalid_emails_to_blank
+    where("email != '' AND email IS NOT NULL and email NOT LIKE '%@%'").update(email: nil)
+  end
+  def self.delete_meggie_herman
+    find_by(:first => 'Meggie', :last => 'Herman').destroy
+  end
+  def self.delete_everyone_born_before_1978
+    where("birthdate < ?", "1978-01-01").destroy_all
+  end
 end
